84ebf1e96169f0ba04c83de0a47fb6b2c2549a82
Sebijuan <sjuacoc2t@adaits.es>
2024-10-03 19:16:07 +0200

    Update ejercicio5.js

--- T1/SPRINTS/ejercicio5.js
+++ T1/SPRINTS/ejercicio5.js
@@ -21,41 +21,41 @@ function getXPath(element) {
     }
 }
 
-// Escuchar clics en el botón principal
+
 document.getElementById('mainButton').addEventListener('click', function() {
     const xpath = getXPath(this);
     alert(`XPath del Botón Principal: ${xpath}`);
     document.getElementById('xpathOutput').textContent = `XPath: ${xpath}`;
 });
 
-// Obtener el iframe
+
 const iframe = document.getElementById('myIframe');
 
-// Esperar a que el iframe cargue completamente
+
 iframe.onload = function() {
     console.log("Iframe cargado correctamente.");
 
-    // Usar un setTimeout para dar tiempo al iframe a cargarse completamente
+    
     setTimeout(function() {
-        // Intentar acceder al documento del iframe
+       
         const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
 
         if (iframeDocument) {
             console.log("Accediendo al iframe:", iframe);
-            // Obtener el botón dentro del iframe
+           
             const iframeButton = iframeDocument.getElementById('iframeButton');
 
             if (iframeButton) {
                 console.log("Botón dentro del iframe encontrado.");
-                // Agregar el evento de clic al botón dentro del iframe
+               
                 iframeButton.addEventListener('click', function(event) {
-                    event.stopPropagation(); // Detener la propagación del evento
+                    event.stopPropagation(); 
 
-                    // Generar XPath para el botón dentro del iframe
+                   
                     const xpath = getXPath(event.target);
                     alert('XPath del botón dentro del iframe: ' + xpath);
 
-                    // Actualizar el texto del párrafo en el HTML principal
+                   
                     document.getElementById('xpathOutput').textContent = 'XPath dentro del iframe: ' + xpath;
                 });
             } else {
@@ -64,10 +64,10 @@ iframe.onload = function() {
         } else {
             console.error("No se pudo acceder al contenido del iframe.");
         }
-    }, 500); // Esperar medio segundo para asegurarse de que el iframe se haya cargado completamente
+    }, 500); 
 };
 
-// Detectar cuando el documento principal pierde el foco
+
 document.addEventListener('visibilitychange', function() {
     if (document.hidden) {
         console.log('El documento principal ha perdido el foco.');
