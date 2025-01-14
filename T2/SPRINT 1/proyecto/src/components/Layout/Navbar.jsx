df53a620bd0b504781880b369d532803526e96b2
Sebijuan <sjuacoc2t@adaits.es>
2025-01-14 06:10:51 +0100

    Update Navbar.jsx

--- T2/SPRINT 1/proyecto/src/components/Layout/Navbar.jsx
+++ T2/SPRINT 1/proyecto/src/components/Layout/Navbar.jsx
@@ -26,10 +26,10 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>SJ XTREMEMODS</h1>
       <ul>
-        <li><a href="#home" onClick={handleHomeClick}>Inicio</a></li>
-        <li><a href="#about" onClick={handleAboutClick}>Sobre Nosotros</a></li>
-        <li><a href="#products" onClick={handleProductsClick}>Productos</a></li>
-        <li><a href="#contact" onClick={handleContactClick}>Contacto</a></li>
+        <li><a href="#home" onClick={handleHomeClick} className="navbar-link">Inicio</a></li>
+        <li><a href="#about" onClick={handleAboutClick} className="navbar-link">Sobre Nosotros</a></li>
+        <li><a href="#products" onClick={handleProductsClick} className="navbar-link">Productos</a></li>
+        <li><a href="#contact" onClick={handleContactClick} className="navbar-link">Contacto</a></li>
       </ul>
       <div className="navbar-cart">
         <img src="src/components/assets/icons/icon.jpg" alt="Carrito" />
