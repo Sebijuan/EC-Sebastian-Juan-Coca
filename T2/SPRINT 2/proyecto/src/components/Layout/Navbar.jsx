59a16b5be694ad4bcc8c262e8fb48f9c53ae25fb
Sebijuan <sjuacoc2t@adaits.es>
2025-01-24 20:05:02 +0100

    Update Navbar.jsx

--- T2/SPRINT 2/proyecto/src/components/Layout/Navbar.jsx
+++ T2/SPRINT 2/proyecto/src/components/Layout/Navbar.jsx
@@ -35,6 +35,14 @@ const Navbar = () => {
     window.location.href = '/register-page';
   };
 
+  const handleAemetClick = (event) => {
+    event.preventDefault();
+    window.location.href = '/aemet';
+  };
+
+
+
+
   return (
     <nav className="navbar navbar-visible">
       <h1>SJ CUSTOMS</h1>
@@ -45,6 +53,7 @@ const Navbar = () => {
         <li><a href="/contacts-page" onClick={handleContactClick} className="navbar-link">Contacto</a></li>
         <li><a href="/login-page" onClick={handleLoginClick} className="navbar-link">Iniciar Sesión</a></li>
         <li><a href="/register-page" onClick={handleRegisterClick} className="navbar-link">Registrate</a></li>
+        <li><a href="/aemet" onClick={handleAemetClick} className="navbar-link">Ver Tiempo</a></li>
       </ul>
       <div className="navbar-icon">
         <img src="/assests/icons/icon.jpg" alt="Icono" />
