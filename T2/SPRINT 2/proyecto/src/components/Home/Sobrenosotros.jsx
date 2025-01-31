2f5e491f9053ea1d410ec41eceddd4a0446e602c
Sebijuan <sjuacoc2t@adaits.es>
2025-01-31 19:49:44 +0100

    actualizacion

--- T2/SPRINT 2/proyecto/src/components/Home/Sobrenosotros.jsx
+++ T2/SPRINT 2/proyecto/src/components/Home/Sobrenosotros.jsx
@@ -4,6 +4,7 @@ import '../styles/SobreNosotros.css';
 const Sobrenosotros = () => {
     return (
         <div className="sobrenosotros-container">
+            {/* Imagen 1 */}
             <img
                 src="/assests/images/sobrenosotros.jpg" 
                 alt="Nosotros"
@@ -14,10 +15,22 @@ const Sobrenosotros = () => {
                 Bienvenidos a nuestra empresa de tuneo y personalización de coches. Nos especializamos en transformar vehículos ordinarios en obras maestras únicas y personalizadas. 
                 Con años de experiencia en la industria automotriz, nuestro equipo de expertos está dedicado a ofrecer servicios de la más alta calidad para satisfacer las necesidades y deseos de nuestros clientes.
             </p>
+            {/* Imagen 2 */}
+            <img
+                src="/assests/images/sobrenosotros2.webp" 
+                alt="Servicio 1"
+                className="sobrenosotros-image"
+            />
             <p>
                 Ofrecemos una amplia gama de servicios que incluyen modificaciones de rendimiento, mejoras estéticas, instalación de sistemas de audio de alta fidelidad, y mucho más. 
                 Utilizamos solo los mejores materiales y las últimas tecnologías para asegurar que cada proyecto sea un éxito.
             </p>
+            {/* Imagen 3 */}
+            <img
+                src="/assests/images/sobrenosotros3.webp" 
+                alt="Servicio 2"
+                className="sobrenosotros-image"
+            />
             <p>
                 Nuestro compromiso es proporcionar un servicio excepcional y resultados que superen las expectativas. Ya sea que desees mejorar el rendimiento de tu coche, darle un nuevo aspecto, o ambos, estamos aquí para ayudarte a hacer realidad tu visión.
             </p>
