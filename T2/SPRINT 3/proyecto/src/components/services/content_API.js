011af4dcdd962c4001525eaa794ea10c42d21717
Sebastian_Juan <sjuacoc2t@adaits.es>
2025-06-03 02:48:21 +0200

    Update content_API.js

--- T2/SPRINT 3/proyecto/src/components/services/content_API.js
+++ T2/SPRINT 3/proyecto/src/components/services/content_API.js
@@ -1,6 +1,6 @@
 import{API_BASE_URL} from './auth_API.js';
 export const fetchProducts = async () => {
-  const response = await fetch(`${API_BASE_URL}api/car/`);
+  const response = await fetch(`${API_BASE_URL}/api/car`);
   if (!response.ok) throw new Error('Error al obtener los productos');
   return await response.json();
 };
